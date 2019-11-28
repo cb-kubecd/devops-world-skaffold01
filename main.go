891be4f5656891a49191d0a9b4e215555e18d71d
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-11-28 11:53:58 +0000

    chore: updated text

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example - A change"
+	title := "Jenkins X golang http examplee"
 
 	from := ""
 	if r.URL != nil {
